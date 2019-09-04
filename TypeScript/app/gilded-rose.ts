3a714f93d0f1508aae88b61d08c2bc2679efc341
Oliver Lloyd <email@oliverlloyd.com>
2019-09-04 12:29:56 +0100

    Replace for loop with a map

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -28,60 +28,56 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name === AGED_BRIE ||
-        this.items[i].name === BACKSTAGE_PASS
-      ) {
-        if (this.items[i].quality < MAX_QUALITY) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == BACKSTAGE_PASS) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < MAX_QUALITY) {
-                this.items[i].quality = this.items[i].quality + 1;
+    return this.items.map(item => {
+      if (item.name === AGED_BRIE || item.name === BACKSTAGE_PASS) {
+        if (item.quality < MAX_QUALITY) {
+          item.quality = item.quality + 1;
+          if (item.name == BACKSTAGE_PASS) {
+            if (item.sellIn < 11) {
+              if (item.quality < MAX_QUALITY) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < MAX_QUALITY) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < MAX_QUALITY) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       } else {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFURAS) {
-            this.items[i].quality = this.items[i].quality - 1;
+        if (item.quality > 0) {
+          if (item.name != SULFURAS) {
+            item.quality = item.quality - 1;
           }
         }
       }
 
-      if (this.items[i].name === SULFURAS) {
+      if (item.name === SULFURAS) {
         // Nothing
       } else {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        item.sellIn = item.sellIn - 1;
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name === AGED_BRIE) {
-          if (this.items[i].quality < MAX_QUALITY) {
-            this.items[i].quality = this.items[i].quality + 1;
+      if (item.sellIn < 0) {
+        if (item.name === AGED_BRIE) {
+          if (item.quality < MAX_QUALITY) {
+            item.quality = item.quality + 1;
           }
         } else {
-          if (this.items[i].name === BACKSTAGE_PASS) {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+          if (item.name === BACKSTAGE_PASS) {
+            item.quality = item.quality - item.quality;
           } else {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS) {
-                this.items[i].quality = this.items[i].quality - 1;
+            if (item.quality > 0) {
+              if (item.name != SULFURAS) {
+                item.quality = item.quality - 1;
               }
             }
           }
         }
       }
-    }
 
-    return this.items;
+      return item;
+    });
   }
 }
