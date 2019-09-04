ad77e5d373b41b9ed87068644a37aa2b596a9f06
Oliver Lloyd <email@oliverlloyd.com>
2019-09-04 13:16:00 +0100

    Fix broken function name

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -22,5 +22,5 @@ for (let i = 0; i < days; i++) {
     console.log(element.name + " " + element.sellIn + " " + element.quality);
   });
   console.log();
-  gildedRose.updateQuality();
+  gildedRose.endOfDayBatch();
 }
