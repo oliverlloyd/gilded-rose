30f26f6027f63883c0d661c49e2233f1cf659285
Oliver Lloyd <email@oliverlloyd.com>
2019-09-04 12:35:48 +0100

    Break out quality adjustments into a function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -28,6 +28,13 @@ export class GildedRose {
   }
 
   updateQuality() {
+    const adjustQuality = (item: Item, amount: number) => {
+      item.quality += amount;
+      if (item.quality > MAX_QUALITY) item.quality = MAX_QUALITY;
+      if (item.quality < 0) item.quality = 0;
+      return item;
+    };
+
     return this.items.map(item => {
       if (item.name === SULFURAS) {
         return item;
@@ -35,17 +42,13 @@ export class GildedRose {
 
       if (item.name === AGED_BRIE || item.name === BACKSTAGE_PASS) {
         if (item.quality < MAX_QUALITY) {
-          item.quality = item.quality + 1;
+          adjustQuality(item, 1);
           if (item.name == BACKSTAGE_PASS) {
             if (item.sellIn < 11) {
-              if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1;
-              }
+              adjustQuality(item, 1);
             }
             if (item.sellIn < 6) {
-              if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1;
-              }
+              adjustQuality(item, 1);
             }
           }
         }
@@ -59,16 +62,12 @@ export class GildedRose {
 
       if (item.sellIn < 0) {
         if (item.name === AGED_BRIE) {
-          if (item.quality < MAX_QUALITY) {
-            item.quality = item.quality + 1;
-          }
+          adjustQuality(item, 1);
         } else {
           if (item.name === BACKSTAGE_PASS) {
             item.quality = item.quality - item.quality;
           } else {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1;
-            }
+            adjustQuality(item, -1);
           }
         }
       }
